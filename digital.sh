14c67dd3da05052dcd46ba1c845c8b334c968ba5
Daniel Worthington-Bodart <dan@bodar.com>
2015-08-27 14:55:29 +0100

    Added packet (generic) support

--- digital.sh
+++ digital.sh
@@ -1,6 +1,6 @@
 docker-machine create \
         --digitalocean-access-token ${DIGITAL_OCEAN_TOKEN} \
-	--digitalocean-size 16gb \
+	--digitalocean-size 64gb \
 	-driver digitalocean \
 	crazyfastbuild
 
---
+++ packet.sh
@@ -0,0 +1,4 @@
+docker-machine create -d generic \
+	--generic-ip-address=147.75.194.51 \
+	crazyfastbuild
+
